8fa262b62b04057555cc063a7bfa5deafce1aa42
Marilyn Lim <marilyn.lim@keysight.com>
2023-01-16 16:30:04 +0800

    My first change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("CNY is coming!");
+        System.out.println("Modified file!");
     }
 
     public String getMessage() {
